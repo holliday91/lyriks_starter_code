d23d89230b8684db636b01ed869967ccb2e3a72d
holliday91 <j.holliday54@gmail.com>
2023-05-05 10:37:09 -0400

    Top_Play_component

--- src/components/TopPlay.jsx
+++ src/components/TopPlay.jsx
@@ -44,94 +44,87 @@ const TopChartCard = ({song, i, isPlaying, activeSong, handlePauseClick, handleP
 const TopPlay = () => {
   const dispatch = useDispatch();
   const { activeSong, isPlaying } = useSelector((state) =>
-  state.player);
-  const {data} = useGetTopChartsQuery();
+    state.player
+  );
+  const { data } = useGetTopChartsQuery();
   const divRef = useRef(null);
   useEffect(() => {
-    divRef.current.scrollIntoView({ behavior: 'smooth'})
-  } );
+    divRef.current.scrollIntoView({ behavior: "smooth" });
+  }, []);
   const topPlays = data?.slice(0, 5);
   const handlePauseClick = () => {
-    dispatch(playPause(false))
+    dispatch(playPause(false));
   };
 
-
-  const handlePLayClick = (song, i) =>{
-    dispatch(setActiveSong({song, data, i }));
+  const handlePLayClick = (song, i) => {
+    dispatch(setActiveSong({ song, data, i }));
     dispatch(playPause(true));
   };
-  return(
-    <div ref={divRef} className="xl:ml-6 ml-0 xl:mb-0 mb-6 flex-1 xl:max-w-[500px] max-w-full flex flex-col">
+  return (
+    <div
+      ref={divRef}
+      className="xl:ml-6 ml-0 xl:mb-0 mb-6 flex-1 xl:max-w-[500px] max-w-full flex flex-col"
+    >
       <div className="w-full flex flex-col">
         <div className="flex flex-row justify-between items-center">
           <h2 className="text-white font-bold text-2xl">Top Charts</h2>
-          <Link to='/top-charts'>
+          <Link to="/top-charts">
             <p className="text-gray-300 text-base cursor-pointer">See more</p>
           </Link>
-
         </div>
         <div className="mt-4 flex flex-col gap-1">
-          {topPlays?.map((song, i) => 
-            <TopChartCard
-              key={song.key}
-              song={song}
-              i={i}
-              isPlaying={isPlaying}
-              activeSong={activeSong}
-              handlePauseClick={ handlePauseClick}
-              handlePLayClick={() => handlePLayClick(song, i)}
-
-            
-            />
-          )}
+        ...
+{topPlays && topPlays.length > 0 &&
+  topPlays.map((song, i) => (
+    <TopChartCard
+      key={song.key}
+      song={song}
+      i={i}
+      isPlaying={isPlaying}
+      activeSong={activeSong}
+      handlePauseClick={handlePauseClick}
+      handlePLayClick={() => handlePLayClick(song, i)}
+    />
+  ))}
+...
 
         </div>
-
       </div>
       <div className="w-full flex flex-col mt-8">
-      <div className="flex flex-row justify-between items-center">
+        <div className="flex flex-row justify-between items-center">
           <h2 className="text-white font-bold text-2xl">Top Artists</h2>
-          <Link to='/top-artists'>
+          <Link to="/top-artists">
             <p className="text-gray-300 text-base cursor-pointer">See more</p>
           </Link>
-
         </div>
-        <Swiper slidesPerView='auto' 
-                spaceBetween={15} 
-                freeMode 
-                centeredSlides 
-                centeredSlidesBounds 
-                modules={[FreeMode]}
-                className='mt-4'>
-
-                  {topPlays?.map((song, i) => (
-                    <SwiperSlide
-                    key={song?.key}
-                    style={{width:'25%', height:'auto'}}
-                    className='shadow-lg rounded-full animate-slideright'
-                    
-                    >
-                      <Link to={` /artists/${song?.artists[0].adamid}`}>
-                        <img src={song?.images.background} alt='name'
-                        className="rounded-full w-full object-cover"/>
-                      
-                      </Link>
-                      
-                    </SwiperSlide>
-                  ))}
-
+        <Swiper
+          slidesPerView="auto"
+          spaceBetween={15}
+          freeMode
+          centeredSlides
+          centeredSlidesBounds
+          modules={[FreeMode]}
+          className="mt-4"
+        >
+          {topPlays?.map((song, i) => (
+            <SwiperSlide
+              key={song?.key}
+              style={{ width: "25%", height: "auto" }}
+              className="shadow-lg rounded-full animate-slideright"
+            >
+              <Link to={`/artists/${song?.artists[0]?.adamid}`}>
+                <img
+                  src={song?.images?.background}
+                  alt={song?.name}
+                  className="rounded-full w-full object-cover"
+                />
+              </Link>
+            </SwiperSlide>
+          ))}
         </Swiper>
       </div>
-
-      <div>
-      
-      </div>
-
-      </div>
-
-    
-
-  )
+    </div>
+  );
 };
 
 export default TopPlay;
--- src/pages/AroundYou.jsx
+++ src/pages/AroundYou.jsx
@@ -1,24 +1,41 @@
-// import { useState, useEffect } from "react";
-// import { Axios } from "axios";
-// import { useSelector } from "react-redux";
-// import { Error, Loader, SongCard } from "../components";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { useSelector } from "react-redux";
+import { Error, Loader, SongCard } from "../components";
 
-// const AroundYou = () => {
-//     const [country, setCountry] = useState('');
-//     const [loading, setLoading] = useState(true);
-//     const (activeSong, isPlaying) = useSelector((state) => state.player);
+const AroundYou = () => {
+    const [country, setCountry] = useState('');
+    const [loading, setLoading] = useState(true);
+    const { activeSong, isPlaying } = useSelector((state) => state.player);
 
-//     useEffect(() =>{
+    useEffect(() => {
+        const fetchLocation = async () => {
+            try {
+                const response = await axios.get('https://ipapi.co/json/');
+                setCountry(response.data.country_name);
+                setLoading(false);
+            } catch (error) {
+                console.error(error);
+                setLoading(false);
+            }
+        };
+        fetchLocation();
+    }, []);
 
-        
-//     })
+    return (
+        <div>
+            {loading ? (
+                <Loader />
+            ) : (
+                <div>
+                    <h1 className="text-white text-2xl font-bold mb-4">Songs Around You in {country}</h1>
+                    {activeSong && isPlaying && <SongCard song={activeSong} />}
+                    <Error />
+                </div>
+            )}
+        </div>
+    );
+};
 
+export default AroundYou;
 
-// return(
-    
-
-// <div>
-
-// </div>)};
-
-// export default AroundYou;
